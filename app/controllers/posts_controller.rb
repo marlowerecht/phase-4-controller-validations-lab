5ab128d6cdf6d4306e79d0ea89f95c88dffc7cf5
Marlowe Recht <recht.marlowe@gmail.com>
2023-02-22 18:00:17 +0100

    commit tree 2ff8b3689a1ad90fdb68ba36285a5f9ec1d02aeb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,29 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     post = Post.find(params[:id])
     
-    render json: post
+    render json: post, status: :ok
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
-    render json: post
+    render json: post, status: :ok
   end
 
   private
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
